024acc33f4e9d6fe3e5eddfb5c2b7c6033cb1c19
ThanhHM2 <thanhhm2@vng.com.vn>
2023-11-24 10:13:45 +0700

    update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://pithagon.com',
-    generateRobotsTxt: true, // (optional)
-  }
+  siteUrl: process.env.SITE_URL || "https://pithagon.com",
+  changefreq: "daily",
+  generateRobotsTxt: true, // (optional)
+};
